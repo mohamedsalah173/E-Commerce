5fb9092d7b70e1da473fd57daf275950418a4ae4
NADAHISHAM22 <nada.hisham.elsaid@gmail.com>
2023-05-01 02:52:58 +0300

    create categoryModel

--- ecomm/categories/models.py
+++ ecomm/categories/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Categories (models.Model) :
+      name        = models.CharField(max_length=30 ,null=False)
+      description = models.TextField(max_length=150 ,null=False)
+      createdAt   = models.DateField(auto_now_add=True)
+      updateAt    = models.DateField(auto_now_add=True)
+      
+      def __str__(self):
+        return self.name
 
 # Create your models here.
