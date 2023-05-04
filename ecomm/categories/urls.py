e2cdb7b2d514748daf3454d9234d6f13fe60e584
NADAHISHAM22 <nada.hisham.elsaid@gmail.com>
2023-05-04 20:22:02 +0300

    category crud and model

---
+++ ecomm/categories/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .api import views
+
+
+
+
+urlpatterns = [
+    
+    path('addcategory',views.add_category),
+    path('getAllCategories',views.getAllCategories),
+    path('getCategoryById/<int:id>',views.getCategoryById)
+    
+]
