96ac494e9fbfba2602475192dbe39e93dfdb407f
Amani khaled <Amani.khaled.mohamed@gmail.com>
2023-05-07 01:23:32 +0300

    add permissions

--- ecomm/categories/api/views.py
+++ ecomm/categories/api/views.py
@@ -4,56 +4,56 @@ from categories.models import Categories
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 
 
-@api_view(['POST','GET'])
+@api_view(['POST', 'GET'])
 def add_category(request):
-    
-        serializer = categoriesSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            data = serializer.data
-            return JsonResponse({'data': data})
-        else :
-            return Response(serializer.errors)
-        
-@api_view(['GET','PUT','DELETE'])
+
+    serializer = categoriesSerializers(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        data = serializer.data
+        return JsonResponse({'data': data})
+    else:
+        return Response(serializer.errors)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def getCategoryById(request, id):
     try:
         category = Categories.objects.get(id=id)
-    except Categories.DoesNotExist :
-            return Response("notfound")
+    except Categories.DoesNotExist:
+        return Response("notfound")
     if request.method == 'GET':
-        
+        permission_classes = [AllowAny]
         serializer = categoriesSerializers(category)
         return JsonResponse(serializer.data)
     if request.method == 'PUT':
-        
+        permission_classes = [IsAdminUser]
         serializer = categoriesSerializers(category, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
-        else :
+        else:
             return JsonResponse(serializer.errors)
-        
-    if request.method == 'DELETE':
-          try:
-             category.delete()
-             return Response("Deleted")
-             
-          except:
-             return Response(category.errors)
-            
 
-    
-    
+    if request.method == 'DELETE':
+        permission_classes = [IsAdminUser]
+        try:
+            category.delete()
+            return Response("Deleted")
+
+        except:
+            return Response(category.errors)
+
+
 @api_view(['GET'])
 def getAllCategories(request):
-     try:
+    permission_classes = [AllowAny]
+    try:
         category = Categories.objects.all()
-     except Categories.DoesNotExist:
-            return Response("notfound")
-     serializer = categoriesSerializers(category, many=True)
-     return Response(serializer.data)
-    
-    
+    except Categories.DoesNotExist:
+        return Response("notfound")
+    serializer = categoriesSerializers(category, many=True)
+    return Response(serializer.data)
--- ecomm/products/api/views.py
+++ ecomm/products/api/views.py
@@ -4,20 +4,20 @@ from rest_framework.response import Response
 from rest_framework.decorators import APIView
 from products.api.serializers import ProductSerializer
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
 
 class productListAV(APIView):
 
-    # permission_classes = [IsAuthenticated]
-
     def get(self, request):
+        permission_classes = [AllowAny]
         products = Product.objects.all()
         serializer = ProductSerializer(
             products, many=True, context={'request': request})
         return Response(serializer.data)
 
     def post(self, request):
+        permission_classes = [IsAdminUser]
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -27,7 +27,9 @@ class productListAV(APIView):
 
 
 class productDetailAV(APIView):
+
     def get(self, request, pk):
+        permission_classes = [AllowAny]
         try:
             product = Product.objects.get(pk=pk)
         except Product.DoesNotExist:
@@ -36,6 +38,7 @@ class productDetailAV(APIView):
         return Response(serializer.data)
 
     def put(self, request, pk):
+        permission_classes = [IsAdminUser]
         try:
             product = Product.objects.get(pk=pk)
         except Product.DoesNotExist:
@@ -50,6 +53,7 @@ class productDetailAV(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
+        permission_classes = [IsAdminUser]
         product = Product.objects.get(pk=pk)
         product.delete()
         return Response({'message': 'Product deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
