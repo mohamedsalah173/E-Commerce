c59f45f2340293c346a82eebf1a3f122cc3ad1f6
MontaserHassan <montaser.hassan.bns@gmail.com>
2023-05-16 16:22:32 +0300

    connect with sqlite

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'ecomm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-user = os.environ.get('DB_USER')
-password = os.environ.get('DB_PASSWORD')
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Django',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': user,
-        'PASSWORD': password,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# user = os.environ.get('DB_USER')
+# password = os.environ.get('DB_PASSWORD')
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Django',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'USER': user,
+#         'PASSWORD': password,
+#     }
+# }
 
 
 # Password validation
