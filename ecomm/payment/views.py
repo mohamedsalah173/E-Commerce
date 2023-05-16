e18e00e2f0d7b8c980fa8400372115b510e2a323
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-05-16 16:28:18 +0300

    modify stripe

--- ecomm/payment/views.py
+++ ecomm/payment/views.py
@@ -1,87 +1,41 @@
-# from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
-# from django.http import HttpResponse, JsonResponse
-# from django.contrib.auth import get_user_model
-# from django.views.decorators.csrf import csrf_exempt
-# import braintree
-
-
-# # Create your views here.
-
-# gateway = braintree.BraintreeGateway(
-#     braintree.Configuration(
-#         braintree.Environment.Sandbox,
-#         merchant_id="8kwh5rb3nzygncb3",
-#         public_key="mxzkmfs7qcn55vhv",
-#         private_key="78bf25be968fc4ecd7c30f6111fd8fd0"
-#     )
-# )
-
-# def validate_user_session(id, token):
-#     UserModel = get_user_model()
-#     try:
-#         user = UserModel.objects.get(pk=id)
-#         if user.session_token == token:
-#             return True
-#     except:
-#         if UserModel.DoesNotExist():
-#             return False
-
-# @csrf_exempt
-# def generate_token(request, id, token):
-#     if not validate_user_session(id, token):
-#         JsonResponse({'error':'you must login again please !'})
-        
-#     return JsonResponse({'client_token':gateway.client_token.generate(), 'success':True})
-
-# @csrf_exempt
-# def payment_method(request, id, token):
-#     if not validate_user_session(id, token):
-#         JsonResponse({'error':'you must login again please !'})
-        
-#     nonce_from_the_client = request.POST['paymentMethodNonce']
-#     amount_from_the_client = request.POST['paymentMethodNonce']
-    
-#     result = gateway.transaction.sale({
-#     "amount": amount_from_the_client,
-#     "payment_method_nonce": nonce_from_the_client,
-#     "options": {
-#       "submit_for_settlement": True
-#     }
-#     })
-    
-#     if result.is_succes:
-#         return JsonResponse({
-#             'success':result.is_success,
-#             'transaction':{
-#                 'id':result.transaction.id,
-#                 'amount':result.transaction.amount,
-#             }
-#             })
-#     else:
-#         JsonResponse({'error':True, 'success':False})
-#! /usr/bin/env python3.6
-
-
 import stripe
 from django.conf import settings
 from rest_framework import status
 from django.shortcuts import  redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from order.views import OrderItemsList;
 
 
 stripe.api_key = settings.STRIP_SECRETE_KEY
 
 class StripeCheckOutView(APIView):
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         try:
+            order_id = 123  # Replace with the actual order ID
+            response = request.get(f'{settings.SITE_URL}/order/{order_id}')
+            
+            if response.status_code == status.HTTP_200_OK:
+                order_data = response.json()
+                order_price = order_data['price']
+                order_name = order_data['name']
+                order_image = order_data['image']
+            else:
+                return Response({'error': 'Failed to retrieve order details'}, status=response.status_code)
             checkout_session = stripe.checkout.Session.create(
+                
                 line_items=[
                     {
-                        'price': 'price_1N8LQ7DIajLiykdApvtl0hZ6',
+                        'price_data': {
+                            'currency': 'usd',
+                            'unit_amount': int(order_price) * 100,
+                            'product_data': {
+                                'name': order_name,
+                                'images': [order_image],
+                            },
+                        },
                         'quantity': 1,
-                    },
+                    },        
                 ],
                 # payment_method_types=['card'],
                 mode='payment',
