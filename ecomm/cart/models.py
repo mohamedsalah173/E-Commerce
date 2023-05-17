8c7d97c89c94af4e9fdcb94bda76b50b4e16724b
NADAHISHAM22 <nada.hisham.elsaid@gmail.com>
2023-05-18 00:15:25 +0300

    update

--- ecomm/cart/models.py
+++ ecomm/cart/models.py
@@ -7,11 +7,12 @@ from django.core.validators import MinValueValidator
 class Cart (models.Model):
     user     = models.ForeignKey(UserBase, on_delete=models.CASCADE)
     products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.user.username
     
 class CartItems (models.Model):
-   
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     
